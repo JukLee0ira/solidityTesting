1c559450523ab0f34e025f7c04f3046af43348fd
JukLee0ira <meebookMonkey@163.com>
2025-03-06 17:30:23 +0800

    clear

--- test/test_v24.ts
+++ test/test_v24.ts
@@ -1,5 +1,4 @@
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
-// import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";TODO: need to rm
 import { expect } from "chai";
 import { ethers } from "hardhat";
 import "@nomicfoundation/hardhat-chai-matchers";
