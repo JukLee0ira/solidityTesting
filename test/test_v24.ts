c6703ae4d1b44b8362fd742c9ffef28f08098d94
JukLee0ira <meebookMonkey@163.com>
2025-03-07 14:16:01 +0800

    se beforeEach

--- test/test_v24.ts
+++ test/test_v24.ts
@@ -1,41 +1,30 @@
-import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 import "@nomicfoundation/hardhat-chai-matchers";
 
 describe("BlobFeatureTest", function () {
-  // 部署合约的fixture
-  async function deployBlobFeatureTestFixture() {
+  // 修改为普通的 beforeEach 部署方式
+  let blobFeatureTest: any;
+
+  beforeEach(async function () {
     const BlobFeatureTest = await ethers.getContractFactory("BlobFeatureTest");
-    const blobFeatureTest = await BlobFeatureTest.deploy();
-    return { blobFeatureTest };
-  }
+    blobFeatureTest = await BlobFeatureTest.deploy();
+    console.log("address", blobFeatureTest.address);
+  });
 
   describe("基础功能测试", function () {
     it("应该能够获取chainId", async function () {
-      const { blobFeatureTest } = await loadFixture(
-        deployBlobFeatureTestFixture
-      );
       const chainId = await blobFeatureTest.testconnect();
-      expect(chainId).to.not.equal(551);
-      // 如果在Hardhat网络上测试，chainId应该是31337 TODO:need to rm
-      expect(chainId).to.equal(31337);
+      expect(chainId).to.equal(63823);
     });
 
     it("应该能够获取blob基础费用", async function () {
-      const { blobFeatureTest } = await loadFixture(
-        deployBlobFeatureTestFixture
-      );
       const blobBaseFee = await blobFeatureTest.testBlobBaseFee();
       // blob基础费用应该是一个非负数
       expect(blobBaseFee).to.be.gte(0);
     });
 
     it("应该能够获取blob哈希", async function () {
-      const { blobFeatureTest } = await loadFixture(
-        deployBlobFeatureTestFixture
-      );
-      // 测试索引0的blob哈希
       const blobHash = await blobFeatureTest.testBlobHash(0);
       // blob哈希应该是一个32字节的值
       expect(blobHash).to.match(/^0x[0-9a-f]{64}$/i);
