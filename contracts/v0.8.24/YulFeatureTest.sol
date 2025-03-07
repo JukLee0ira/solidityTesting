51a3bd0c56fa306e92d54bed1717bbf44c9e80c6
JukLee0ira <meebookMonkey@163.com>
2025-03-07 19:50:03 +0800

    fix

--- contracts/v0.8.24/YulFeatureTest.sol
+++ contracts/v0.8.24/YulFeatureTest.sol
@@ -18,31 +18,4 @@ contract YulFeatureTest {
         return storedValue;
     }
 
-
-
-
-
-        mapping(address => bool) sentGifts;
-    modifier nonreentrant {
-        assembly {
-            if tload(0) { revert(0, 0) }
-            tstore(0, 1)
-        }
-        _;
-        // Unlocks the guard, making the pattern composable.
-        // After the function exits, it can be called again, even in the same transaction.
-        assembly {
-            tstore(0, 0)
-        }
-    }
-    function claimGift() nonreentrant public {
-        require(address(this).balance >= 1 ether);
-        require(!sentGifts[msg.sender]);
-        (bool success, ) = msg.sender.call{value: 1 ether}("");
-        require(success);
-
-        // In a reentrant function, doing this last would open up the vulnerability
-        sentGifts[msg.sender] = true;
-    }
-
 } 
